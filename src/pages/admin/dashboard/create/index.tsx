9b148cb655fbaa6eb36012946862b7ab3a6fe97f
Jack <102869405+jloizel@users.noreply.github.com>
2024-10-20 22:58:03 +0100

    fixing deploy error

--- src/pages/admin/dashboard/create/index.tsx
+++ src/pages/admin/dashboard/create/index.tsx
@@ -8,6 +8,7 @@ import { MdDeleteForever } from "react-icons/md";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import withAuth from '@/utils/withAuth';
 // import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 import dynamic from 'next/dynamic';
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
